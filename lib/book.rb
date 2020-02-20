dfa734c8928693e3022b8fc3f8b993c4bc13f084
github username <github email address>
2020-02-20 18:03:27 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
+# learn sc/01_book_spec.rb
 
+
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
+
+book = Book.new("test")
