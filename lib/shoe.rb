ce2422390da108046632a44d311034a673086868
RBRNFRVR <rommelchavez99@gmail.com>
2020-02-24 16:57:04 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize (brand)
+        @brand = brand
+    end
+    def cobble
+        if  @condition ="new"
+        puts "Your shoe is as good as new!"
+        else  
+            puts "Your #{brand} is repaired!"
+        end
+    end
+end
